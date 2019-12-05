d282e18681e32d05be8e56b587657d1e5127cf91
chensming <654916418@qq.com>
2019-12-05 16:10:38 +0800

    add

---
+++ bookNote/Unit5/J_Add.java
@@ -0,0 +1,15 @@
+
+
+public class J_Add<T>{
+    public String sum(T a1, T a2, T a3){
+        return (a1.toString() + a2.toString() +a3.toString());
+    }
+
+    public static void main(String args[]){
+        J_Add<Integer> b = new J_Add<>();
+        Integer a1 = new Integer(1);
+        Integer a2 = new Integer(2);
+        Integer a3 = new Integer(3);
+        System.out.println(b.sum(a1,a2,a3));
+    }
+}
---
+++ bookNote/Unit5/J_AddInterface.java
@@ -0,0 +1,24 @@
+
+interface J_Interface<T extends Number> {
+    public int mb_Sum(T a1, T a2, T a3);
+}
+
+
+public class J_AddInterface<T extends Number> implements J_Interface<T> {
+    @Override
+    public int mb_Sum(T a1, T a2, T a3) {
+        int b1 = a1.intValue();
+        int b2 = a2.intValue();
+        int b3 = a3.intValue();
+        return (b1 + b2 + b3);
+    }
+
+    public static void main(String args[]) {
+        //J_AddInterface<Integer> b = new J_AddInterface<Integer>()；也可
+        J_AddInterface<Integer> b = new J_AddInterface<>();
+        Integer a1 = new Integer(1);
+        Integer a2 = new Integer(2);
+        Integer a3 = new Integer(3);
+        System.out.println(b.mb_Sum(a1, a2, a3));
+    }
+}
