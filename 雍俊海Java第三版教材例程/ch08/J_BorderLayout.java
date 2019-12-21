a18979ddce0524294f3cc9af178be4f656e5291b
chensming <654916418@qq.com>
2019-12-21 17:24:58 +0800

    add

--- 雍俊海Java第三版教材例程/ch08/J_BorderLayout.java
+++ 雍俊海Java第三版教材例程/ch08/J_BorderLayout.java
@@ -6,76 +6,30 @@
 // ////////////////////////////////////////////////////////
 // 简介:
 //     边界布局管理器例程。
-// ////////////////////////////////////////////////////////
-// Copyright:
 //
-// Using this example, please explicitly refer to the book:
-//     Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//     Beijing: Tsinghua University Press, 2014.
-// The example should be used legally, beneficially and
-// without any harm to anybody. Please note that the
-// author and the publisher make no warranty of any kind
-// on the examples provided.
-// Citation examples:
-// The program is an example in (or based on, or modified from)
-//     Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//     Beijing: Tsinghua University Press, 2014.
-//
-// Some selected books by Jun-Hai Yong are:
-//      [1] Jun-Hai Yong. Playing Magic Cube Is Fun: 
-//          A Small Course by Professor of Tsinghua University.
-//          Beijing: Tsinghua University Press, 2018.
-//      [2] Jun-Hai Yong. Programming in C. 
-//          Beijing: Tsinghua University Press, 2017.
-//      [3] Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//          Beijing: Tsinghua University Press, 2014.
-//      [4] Jun-Hai Yong. Programming in Java (Second Edition). 
-//          Beijing: Tsinghua University Press, 2014.
-//      [5] Jun-Hai Yong. Fundamentals of Computer Animation: Algorithms and Programming. 
-//          Beijing: Tsinghua University Press, 2008.
-//      [6] Jun-Hai Yong. Exercises for Programming in Java.
-//          Beijing: Tsinghua University Press, 2006.
-//
-// 版权:
-// 使用本例子，请注明引用:
 //     雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-// 请合法使用例程，其用途应当合法有益而且不应对任何人造成任何
-// 伤害或损失。同时请注意教材作者及出版社没有对例程做出任何承
-// 诺与保证。
-// 具体引用的方法及例子如下:
-// 本程序是下面教材的一个例程(或本程序基于下面教材的例程修改)
-//     雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-//
-// 雍俊海还编写过如下的教材和教参:
-//      [1] 雍俊海. 清华教授的小课堂: 魔方真好玩. 北京: 清华大学出版社. 2018.
-//      [2] 雍俊海. C程序设计. 北京: 清华大学出版社. 2017.
-//      [3] 雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-//      [4] 雍俊海. Java程序设计(第2版). 北京: 清华大学出版社. 2014.
-//      [5] 雍俊海. 计算机动画算法与编程基础. 北京: 清华大学出版社. 2008.
-//      [6] 雍俊海. Java程序设计习题集(含参考答案). 
-//          北京: 清华大学出版社, 2006.
-//
-// ////////////////////////////////////////////////////////
+
+package swing;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class J_BorderLayout
-{
-    public static void main(String args[ ])
-    {
-        JFrame app = new JFrame( "边界布局管理器例程" );
+public class J_BorderLayout {
+    public static void main(String args[]) {
+        JFrame app = new JFrame("边界布局管理器例程");
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setSize( 360, 130 );
-        Container c = app.getContentPane( );
-        c.setLayout( new BorderLayout( ) ); // 本语句可以删去
+        app.setSize(360, 130);
+        Container c = app.getContentPane();
+        c.setLayout(new BorderLayout()); // 本语句可以删去
         c.add(new JButton("东"), BorderLayout.EAST);
         c.add(new JButton("西"), BorderLayout.WEST);
         c.add(new JButton("南"), BorderLayout.SOUTH);
         c.add(new JButton("北"), BorderLayout.NORTH);
         c.add(new JButton("中"), BorderLayout.CENTER);
-        app.setVisible( true );
+        app.setVisible(true);
     } // 方法main结束
 } // 类J_BorderLayout结束
+
+//J_BoxLayout.java
--- 雍俊海Java第三版教材例程/ch08/J_LabelFrame.java
+++ 雍俊海Java第三版教材例程/ch08/J_LabelFrame.java
@@ -1,61 +1,16 @@
+package swing;
 // ////////////////////////////////////////////////////////
-// 
+//
 // J_LabelFrame.java
-// 
+//
 // 开发者: 雍俊海
 // ////////////////////////////////////////////////////////
 // 简介:
 //     在框架中添加标签的例程。
+//
+// 雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
 // ////////////////////////////////////////////////////////
-// Copyright:
-//
-// Using this example, please explicitly refer to the book:
-//     Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//     Beijing: Tsinghua University Press, 2014.
-// The example should be used legally, beneficially and
-// without any harm to anybody. Please note that the
-// author and the publisher make no warranty of any kind
-// on the examples provided.
-// Citation examples:
-// The program is an example in (or based on, or modified from)
-//     Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//     Beijing: Tsinghua University Press, 2014.
-//
-// Some selected books by Jun-Hai Yong are:
-//      [1] Jun-Hai Yong. Playing Magic Cube Is Fun: 
-//          A Small Course by Professor of Tsinghua University.
-//          Beijing: Tsinghua University Press, 2018.
-//      [2] Jun-Hai Yong. Programming in C. 
-//          Beijing: Tsinghua University Press, 2017.
-//      [3] Jun-Hai Yong. Textbook for Programming in Java (Third Edition). 
-//          Beijing: Tsinghua University Press, 2014.
-//      [4] Jun-Hai Yong. Programming in Java (Second Edition). 
-//          Beijing: Tsinghua University Press, 2014.
-//      [5] Jun-Hai Yong. Fundamentals of Computer Animation: Algorithms and Programming. 
-//          Beijing: Tsinghua University Press, 2008.
-//      [6] Jun-Hai Yong. Exercises for Programming in Java.
-//          Beijing: Tsinghua University Press, 2006.
-//
-// 版权:
-// 使用本例子，请注明引用:
-//     雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-// 请合法使用例程，其用途应当合法有益而且不应对任何人造成任何
-// 伤害或损失。同时请注意教材作者及出版社没有对例程做出任何承
-// 诺与保证。
-// 具体引用的方法及例子如下:
-// 本程序是下面教材的一个例程(或本程序基于下面教材的例程修改)
-//     雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-//
-// 雍俊海还编写过如下的教材和教参:
-//      [1] 雍俊海. 清华教授的小课堂: 魔方真好玩. 北京: 清华大学出版社. 2018.
-//      [2] 雍俊海. C程序设计. 北京: 清华大学出版社. 2017.
-//      [3] 雍俊海. Java程序设计教程(第3版). 北京: 清华大学出版社. 2014.
-//      [4] 雍俊海. Java程序设计(第2版). 北京: 清华大学出版社. 2014.
-//      [5] 雍俊海. 计算机动画算法与编程基础. 北京: 清华大学出版社. 2008.
-//      [6] 雍俊海. Java程序设计习题集(含参考答案). 
-//          北京: 清华大学出版社, 2006.
-//
-// ////////////////////////////////////////////////////////
+
 
 import java.awt.Container;
 import java.awt.FlowLayout;
@@ -63,36 +18,34 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class J_LabelFrame extends JFrame
-{
-    public J_LabelFrame( )
-    {
-        super( "框架和标签例程" );
-        String [ ] s = {"文本标签", "文字在图标的左侧", "文字在图标的下方"};
-        ImageIcon [ ] ic = {null, new ImageIcon( "img1.gif" ),
-            new ImageIcon( "img2.gif" )};
-        int  [ ] ih  = {0, JLabel.LEFT,   JLabel.CENTER};
-        int  [ ] iv  = {0, JLabel.CENTER, JLabel.BOTTOM};
-        Container c = getContentPane( );
-        c.setLayout( new FlowLayout(FlowLayout.LEFT) );
-        for (int i=0; i<3; i++)
-        {
-            JLabel aLabel = new JLabel( s[i] , ic[i], JLabel.LEFT);
-            if (i>0)
-            {
+public class J_LabelFrame extends JFrame {
+    public J_LabelFrame() {
+        super("框架和标签例程");
+        String[] s = {"文本标签", "文字在图标的左侧", "文字在图标的下方"};
+        ImageIcon[] ic = {null, new ImageIcon("img1.gif"),
+                new ImageIcon("img2.gif")};
+        int[] ih = {0, JLabel.LEFT, JLabel.CENTER};
+        int[] iv = {0, JLabel.CENTER, JLabel.BOTTOM};
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout(FlowLayout.LEFT));
+        for (int i = 0; i < 3; i++) {
+            JLabel aLabel = new JLabel(s[i], ic[i], JLabel.LEFT);
+            if (i > 0) {
                 aLabel.setHorizontalTextPosition(ih[i]);
                 aLabel.setVerticalTextPosition(iv[i]);
             } // if结构结束
-            aLabel.setToolTipText( "第" + (i+1) + "个标签");
-            c.add( aLabel );
+            aLabel.setToolTipText("第" + (i + 1) + "个标签");
+            c.add(aLabel);
         } // for循环结束
     } // J_LabelFrame构造方法结束
 
-    public static void main(String args[ ])
-    {
-        J_LabelFrame app = new J_LabelFrame( );
+    public static void main(String args[]) {
+        J_LabelFrame app = new J_LabelFrame();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setSize( 360, 150 );
-        app.setVisible( true );
+        app.setSize(360, 150);
+        app.setVisible(true);
     } // 方法main结束
 } // 类J_LabelFrame结束
+
+
+//J_FrameDialog.java
